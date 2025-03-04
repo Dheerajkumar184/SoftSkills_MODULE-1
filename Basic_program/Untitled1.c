966480c39c060957594de52b5af5063d18dde56d
Dheeraj kumar <dheeraj8021kr@gmail.com>
2025-03-04 17:40:03 +0530

    Delete Basic_program directory

--- Basic_program/Untitled1.c
+++
@@ -1,7 +0,0 @@
-//wap to swap the two numbers.
-#include<stdio.h>
-main()
-{
-	 printf("a=20,b=10");
-	 
-}
--- Basic_program/d._your_address.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-main()
-{
-	printf("Vill-teghra,P|O-Bhiya chaurasta,P|s-Bihiya,dist-bhojpur,pincode-802154(Bihar)");
-}
